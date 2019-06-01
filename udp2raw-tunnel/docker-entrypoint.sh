a8b080256cde74cebb41536a550ad0b06f6e7447
joelei <joelei@tencent.com>
2019-06-01 13:37:42 +0800

    finish udp2raw

--- udp2raw-tunnel/docker-entrypoint.sh
+++ udp2raw-tunnel/docker-entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/bin/udp2raw_amd64 -s -l 0.0.0.0:$PORT -r 127.0.0.1:$PORT -k $KEY --raw-mode faketcp --cipher-mode aes128cbc --auth-mode hmac_sha1
+/usr/local/bin/udp2raw_amd64 -$MODE -l $LISTEN -r $REMOTE -k $KEY --raw-mode faketcp --cipher-mode aes128cbc --auth-mode hmac_sha1
